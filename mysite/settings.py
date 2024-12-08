6f2104179dc63303f3ab129b790182fb8d1014ff
rajneesh-y <rajneeshy568@gmail.com>
2024-12-08 18:54:18 +0530

    staticfiles included

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Add the following in your settings.py
 MEDIA_URL = '/media/'
