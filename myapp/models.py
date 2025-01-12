9891ff46832c44bb2e875eb93877711e6d413d17
rajneesh-y <rajneeshy568@gmail.com>
2025-01-12 14:27:47 +0530

    test profile

--- myapp/models.py
+++ myapp/models.py
@@ -30,3 +30,26 @@ class ShopItem(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
+STATE_CHOICE=(
+    ("UP","Uttar Pradesh"),
+    ("MP","Madhya Pradesh"),
+    ("HP","Himanchal Pradesh"),
+    ("JK","Jammu and Kasmir"),
+    ("BR","Bihar"),
+)
+class Resume(models.Model):
+    name=models.CharField(max_length=100)
+    dob=models.DateField(auto_now=False,auto_now_add=False)
+    gender= models.CharField(max_length=100)
+    locality=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    pin=models.PositiveBigIntegerField()
+    state=models.CharField(choices=STATE_CHOICE,max_length=100)
+    mobile=models.PositiveBigIntegerField()
+    email=models.EmailField()
+    job_city=models.CharField(max_length=100)
+    profile_image=models.ImageField(upload_to="profileimg",blank=True)
+    my_file=models.FileField(upload_to="doc",blank=True)
